128a691357ed5000b420b308f8eb12f34775a6c8
Nikhil Pareek <nikhilpareekpandit@gmail.com>
2025-04-22 22:32:09 +0530

    Created "Choose Mode" Page

--- lib/core/config/assets/app_vectors.dart
+++ lib/core/config/assets/app_vectors.dart
@@ -1,3 +1,9 @@
 class AppVectors{
   static const String logo = 'assets/vectors/spotify_logo.svg';
+  static const String sun = 'assets/vectors/sun.svg';
+  static const String moon = 'assets/vectors/moon.svg';
+  static const String bottomPattern = 'assets/vectors/bottom_pattern.svg';
+  static const String homeTopCard = 'assets/vectors/home_top_card.svg';
+  static const String topPattern = 'assets/vectors/top_pattern.svg';
+
 }
--- lib/presentation/choose_mode/pages/choose_mode.dart
+++ lib/presentation/choose_mode/pages/choose_mode.dart
@@ -1,10 +1,141 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:notspotify/common/widgets/button/basic_app_button.dart';
+import 'package:notspotify/core/config/assets/app_images.dart';
+import 'package:notspotify/core/config/assets/app_vectors.dart';
+import 'package:notspotify/core/config/theme/app_colors.dart';
 
 class ChooseMode extends StatelessWidget {
   const ChooseMode({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(
+              vertical: 40,
+              horizontal: 40,
+            ),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage(AppImages.chooseModeBg),
+                )
+            ),
+          ),
+          Container(
+            // ignore: deprecated_member_use
+            color: Colors.black.withOpacity(0.15),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 40,
+              horizontal: 40,
+            ),
+            child: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: SvgPicture.asset(
+                      AppVectors.logo,
+                    ),
+                  ),
+                  Spacer(),
+                  Text(
+                    'Choose Mode',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                  SizedBox(height: 40,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        children: [
+                          ClipOval(
+                            child: BackdropFilter(
+                              filter: ImageFilter.blur(sigmaX: 10,sigmaY: 10),
+                              child: Container(
+                                height: 80,
+                                width: 80,
+                                decoration: BoxDecoration(
+                                  color: Color(0xff30393C).withOpacity(0.5),
+                                  shape: BoxShape.circle,
+                                ),
+                                child: SvgPicture.asset(
+                                  AppVectors.moon,
+                                  fit: BoxFit.none,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 10,),
+                          Text(
+                            'Dark Mode',
+                            style: TextStyle(
+                              color: AppColors.grey,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 13,
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        width: 40,
+                      ),
+                      Column(
+                        children: [
+                          ClipOval(
+                            child: BackdropFilter(
+                              filter: ImageFilter.blur(sigmaX: 10,sigmaY: 10),
+                              child: Container(
+                                height: 80,
+                                width: 80,
+                                decoration: BoxDecoration(
+                                  color: Color(0xff30393C).withOpacity(0.5),
+                                  shape: BoxShape.circle,
+                                ),
+                                child: SvgPicture.asset(
+                                  AppVectors.sun,
+                                  fit: BoxFit.none,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 10,),
+                          Text(
+                            'Light Mode',
+                            style: TextStyle(
+                              color: AppColors.grey,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 13,
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 50,),
+                  BasicAppButton(
+                    onPressed: (){
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (BuildContext context) => const ChooseMode()),
+                      );
+                    },
+                    title: 'Continue')
+                ],
+              ),
+          ),
+        ],
+      ),
+    );
   }
 }
